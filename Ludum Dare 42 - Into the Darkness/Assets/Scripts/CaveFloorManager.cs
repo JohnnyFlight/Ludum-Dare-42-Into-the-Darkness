ec43b7dfcc7a450af04c314b13bb6925928fa3e3
DukeSplit <daniel.t.gollan@gmail.com>
2018-08-11 16:25:03 +0100

    <Hows This>

---
+++ Ludum Dare 42 - Into the Darkness/Assets/Scripts/CaveFloorManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CaveFloorManager : MonoBehaviour {
+
+
+    int Width;
+    int Height;
+
+    public GameObject[] FloorPrefabs;
+
+    public void FloorCreate(int x, int y) {
+
+        Width = x;
+        Height = y;
+
+        for (int widthLoop = 0; widthLoop < Width; widthLoop++)
+        {
+            for (int heightLoop = 0; heightLoop < Height; heightLoop++)
+            {
+                GameObject toInstantiate = FloorPrefabs[Random.Range(0, FloorPrefabs.Length)];
+                GameObject instance = Instantiate(toInstantiate, new Vector3(widthLoop, heightLoop, 0f), Quaternion.identity) as GameObject;
+            }
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Ludum Dare 42 - Into the Darkness/Assets/Scripts/CellManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellManager : MonoBehaviour {
+
+    // Use this for initialization
+
+    int Width;
+    int Height;
+
+    ArrayList Rows;
+
+    struct cell {
+
+        public int x;
+        public int y;
+        public GameObject TileInfo;
+
+    }
+
+    public void CellCreate(int x, int y) {
+        Width = x;
+        Height = y;
+
+        Rows = new ArrayList();
+        
+        for (int rowsLoop = 0; rowsLoop < Width; rowsLoop++)
+        {
+            ArrayList Columns = new ArrayList();
+            for (int columnsLoop = 0; columnsLoop < Height; columnsLoop++)
+            {
+                cell newCell;
+                newCell.x = rowsLoop;
+                newCell.y = columnsLoop;
+                newCell.TileInfo = null;
+                Columns.Add(newCell);
+            }
+            Rows.Add(Columns);
+
+        }
+    }
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
